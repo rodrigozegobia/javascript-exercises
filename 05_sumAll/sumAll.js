cc37101d1f2963703272f7dfa6af67a1131c48d8
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-02-07 15:18:14 -0300

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num,num2) {
+    if (!Number.isInteger(num) || !Number.isInteger(num2)) return "ERROR";
+    if (num < 0 || num2 < 0) return "ERROR";
+    if (num > num2) {
+        const temp = num;
+        num = num2;
+        num2 = temp;
+    }
+    let sum = 0;
+    for (let i = num; i < num2 + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
